a3e88eaf47bd460ec867ba8cad77111f3f35de19
Anthony Dentinger <a.dentinger@gmail.com>
2022-09-30 23:32:41 -0400

    Add UI packages

--- ademacs.d/init.el
+++ ademacs.d/init.el
@@ -16,9 +16,27 @@
 
 (setq visible-bell t)
 
+; Load temporary theme while the real theme is being downloaded
+; or loaded
 (load-theme 'wombat)
 
+; Font size
+(set-face-attribute 'default nil :height 170)
+
+; Don't add the annoying Custom line things in this file; add
+; them to a separate file
 (setq custom-file (concat user-emacs-directory "/custom.el"))
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
+; Don't show line number in certain modes
+(dolist (mode '(org-mode-hook
+                term-mode-hook
+                shell-mode-hook
+                treemacs-mode-hook
+                eshell-mode-hook))
+  (add-hook mode (lambda () (display-line-numbers-mode 0))))
 
 ; Need package stuff for use-package!
 (require 'package)
@@ -28,7 +46,7 @@
 ; Put the packages inside the current emacs dir, not the default one.
 (setq package-user-dir (concat user-emacs-directory "/packages"))
 (package-initialize)
-; Only do that on initial package setupl
+; Only do that on initial package setup
 (unless package-archive-contents (package-refresh-contents))
 ; For non-Linux platforms apparently...
 (unless (package-installed-p 'use-package) (package-install 'use-package))
@@ -55,9 +73,46 @@
   :config
   (ivy-mode 1))
 
+; Counsel package is used by ivy-rich; ivy-rich doesn't seem to work without it
+(use-package counsel
+  :bind (("M-x" . counsel-M-x)
+	 ("C-x b" . counsel-ibuffer)
+	 ("C-x C-f" . counsel-find-file)
+	 :map minibuffer-local-map ("C-r" . 'counsel-minibuffer-history))
+  :config (setq ivy-initial-inputs-alist nil) ; Don't start searches with "^"
+)
+
+(use-package ivy-rich
+  :init (ivy-rich-mode 1))
+
 (use-package all-the-icons)
 
 (use-package doom-modeline
   :ensure t
   :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
+  :custom ((doom-modeline-height 2)))
+
+(use-package doom-themes)
+
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
+
+; To show available keybindings when using a hierarchical keybinding
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config (setq which-key-idle-delay 0.3))
+
+(use-package helpful
+  ; :custom section is to avoid adding variables in the annoying custom.el file
+  ; that we setup earlier
+  :custom
+  (counsel-describe-function-function #'helpful-callable)
+  (counsel-describe-variable-function #'helpful-variable)
+  :bind
+  ; remap thing apparently returns the string representing the keybinding that
+  ; binds given function
+  ([remap describe-function] . counsel-describe-function)
+  ([remap describe-command] . helpful-command)
+  ([remap describe-variable] . counsel-describe-variable)
+  ([remap describe-key] . helpful-key))
