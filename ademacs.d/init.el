d71fce09c9b12fa67f33cf284e9a3a98b389e9ca
Anthony Dentinger <a.dentinger@gmail.com>
2022-12-04 22:55:00 -0500

    Start lsp on sh-mode

--- ademacs.d/init.el
+++ ademacs.d/init.el
@@ -753,6 +753,7 @@ before but had already worked."
   (lsp-mode . ade/lsp-mode-setup)
   (c-mode   . ade/lsp-c-c++-mode-setup)
   (c++-mode . ade/lsp-c-c++-mode-setup)
+  (sh-mode . lsp-deferred)
   :config
   (lsp-enable-which-key-integration t)
   (bind-key (concat ade/generic-pfx-plain " C-c") #'completion-at-point nil nil))
